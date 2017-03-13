955d80d86cebfd59a8e1aa5e1389662401b1dec1
bticknor <bticknor@adharmonics.com>
2017-03-12 21:04:26 -0400

    start work on run_bot_server.py

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ import os
 config = ConfigParser.ConfigParser()
 config.read([os.path.expanduser('~/.tldr_slackbot_conf')])
 try:
-    slack_api_key = config.get('Slack API Credentials', 'api_key')
+    bot_token = config.get('Slack API Credentials', 'bot_token')
     bot_id = config.get('Slack API Credentials', 'bot_id')
     smmry_api_key = config.get('SMMRY API Credentials', 'api_key')
 except ConfigParser.NoSectionError:
-    slack_api_key = None
+    bot_token = None
     bot_id = None
     smmry_api_key = None
 finally:
     TLDR_BOT_CONFIG = {
-        'slack_api_key': slack_api_key,
+        'bot_token': bot_token,
         'bot_id': bot_id,
         'smmry_api_key': smmry_api_key
     }
---
+++ run_bot_server.py
@@ -0,0 +1,59 @@
+from slackclient import SlackClient
+import time
+from config import TLDR_BOT_CONFIG
+from request_smmry import (
+    make_request,
+    parse_response
+)
+
+
+# SMMRY API key
+SMMRY_API_KEY = TLDR_BOT_CONFIG['smmry_api_key']
+# bot id
+BOT_ID = TLDR_BOT_CONFIG['bot_id']
+# slack API key
+BOT_TOKEN = TLDR_BOT_CONFIG['bot_token']
+# read from event firehose at 1 second intervals
+READ_WEBSOCKET_DELAY = 1
+
+
+## todo: need to identify which channel bot was summoned from,
+## todo: then parse that channel's history for what to summarize
+
+
+def parse_slack_activity(rtm_output):
+    """Parses output from slack RTM event firehose.
+
+    :param rtm_output: firehose event output
+    :type rtm_output: list
+
+    :return TBD:
+    :rtype: TBD
+    """
+    if rtm_output and len(rtm_output) > 0:
+        for event in rtm_output:
+           ## debug
+           print "event is", event
+
+            # parse event
+    return None
+
+
+def run_bot_server():
+    """Runs the bot server."""
+    # instantiate slack client
+    client = SlackClient(BOT_TOKEN)
+    connected = client.rtm_connect()
+    if not connected:
+        raise RuntimeError('Slack API connection failed')
+    while True:
+        ## do bot stuff
+        rtm_output = client.rtm_read()
+        parse_slack_activity(rtm_output)
+        ## end do bot stuff
+        time.sleep(READ_WEBSOCKET_DELAY)
+
+
+if __name__ == '__main__':
+    run_bot_server()
+
